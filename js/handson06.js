9ef46e864312343100b27a17c501f8a114388320
Aluno <aluno@edu.senac.br>
2023-08-30 21:13:34 -0300

    testando nuvem

---
+++ js/handson06.js
@@ -0,0 +1,8 @@
+ function impapar(){
+    var numero=parseInt(document.getElementById("ip").value)
+    if (numero % 2 == 0){
+        alert("par")
+   }else{ 
+     alert("impar")
+   }
+ }
---
+++ js/handson07.js
@@ -0,0 +1,11 @@
+ function mediadanota(){
+    var nota1 = parseInt(document.getElementById('nota1').value)
+    var nota2 = parseInt(document.getElementById('nota2').value)
+    var nota3 = (nota1 + nota2) / 2
+  
+    if (nota1 >= 5) {
+      alert(nota3 + " Aprovado ")
+    } else {
+      alert(nota3 + " Reprovado ")
+    }
+  }
